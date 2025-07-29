d03f050e13c63a9838e3e367067b6e27541a9f1b
Kiprov <118040079+Kiprov@users.noreply.github.com>
2025-07-29 14:57:41 +0300

    Update Source.lua

--- Modules/VisualIndicator/Source.lua
+++ Modules/VisualIndicator/Source.lua
@@ -8,12 +8,33 @@ Ported by Kip.
 if getgenv().VisualIndicator then
 return getgenv().VisualIndicator
 end
-local plr = game.Players.LocalPlayer
-local ui = Instance.new("ScreenGui",plr.PlayerGui)
+local cloneref = (cloneref or clonereference or function(instance: any) return instance end)
+local CoreGui: CoreGui = cloneref(game:GetService('CoreGui'))
+local Players: Players = cloneref(game:GetService('Players'))
+local RunService: RunService = cloneref(game:GetService('RunService'))
+local TweenService: TweenService = cloneref(game:GetService('TweenService'))
+local HttpService: HttpService = cloneref(game:GetService('HttpService'))
+local Debris: Debris = cloneref(game:GetService('Debris'))
+local RenderStepped = RunService.RenderStepped
+local LocalPlayer = Players.LocalPlayer
+
+local ProtectGui = protectgui or (function() end)
+local GetHUI = gethui or (function() return CoreGui end)
+local assert = function(condition, errorMessage) 
+    if (not condition) then
+        error(if errorMessage then errorMessage else "assert failed", 3)
+    end
+end
+local plr = LocalPlayer
+local ui = Instance.new("ScreenGui")
 ui.Name = "Visuals"
 ui.IgnoreGuiInset = true
 ui.ResetOnSpawn = false
 ui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+ui.ClipToDeviceSafeArea = false
+ui.DisplayOrder = 999
+pcall(ProtectGui, ui)
+ui.Parent = GetHUI()
 local preloadService = loadstring(game:HttpGet("https://raw.githubusercontent.com/Kiprov/Utilities/refs/heads/main/Modules/PreloadService/Source.lua"))()
 preloadService:SetDirectory("VisualIndicator")
 function preload(assetType,assetURL,assetName,format)
@@ -32,12 +53,12 @@ function module.ActivateCircle(part, range, length, color)
 		circleUI.Visible = true
 		circleUI.ImageTransparency = 1
 		circleUI.ImageColor3 = color or Color3.fromRGB(0, 213, 255)
-		local circleTween = game.TweenService:Create(circleUI, TweenInfo.new(0.5, Enum.EasingStyle.Linear, Enum.EasingDirection.Out, -1, true), {
+		local circleTween = TweenService:Create(circleUI, TweenInfo.new(0.5, Enum.EasingStyle.Linear, Enum.EasingDirection.Out, -1, true), {
 			["ImageTransparency"] = 0.75
 		})
 		circleTween:Play()
-		local circleName = "CircleIndicator" .. game:GetService("HttpService"):GenerateGUID()
-		game["Run Service"]:BindToRenderStep(circleName, 205, function()
+		local circleName = "CircleIndicator" .. HttpService:GenerateGUID()
+		RunService:BindToRenderStep(circleName, 205, function()
 			local char = plr.Character or plr.CharacterAdded:Wait()
 			local hum = char:FindFirstChildWhichIsA("Humanoid")
 			local root = hum.RootPart
@@ -67,7 +88,7 @@ function module.ActivateCircle(part, range, length, color)
 		end)
 		if length then
 			task.delay(length, function()
-				game["Run Service"]:UnbindFromRenderStep(circleName)
+				RunService:UnbindFromRenderStep(circleName)
 				if circleTween then
 					circleTween:Pause()
 					circleTween = nil
@@ -104,11 +125,11 @@ function module.TrackEntity(part, distance, color, length)
 		local entityUI = module.UI.GeneralIndicator:Clone()
 		entityUI.Name = part.Parent.Name
 		entityUI.Parent = module.UI
-	        entityUI.ZIndex = #module.EntityIndicatorUIS
+		entityUI.ZIndex = #module.EntityIndicatorUIS
 		module.EntityIndicatorUIS[part.Parent.Name] = entityUI
 		module.Enableds[part.Parent.Name] = false
-		game["Run Service"]:UnbindFromRenderStep(renderName)
-		game["Run Service"]:BindToRenderStep(renderName, 205, function()
+		RunService:UnbindFromRenderStep(renderName)
+		RunService:BindToRenderStep(renderName, 205, function()
 		local char = plr.Character or plr.CharacterAdded:Wait()
 			local hum = char:FindFirstChildWhichIsA("Humanoid")
 			local root = hum.RootPart
@@ -131,27 +152,27 @@ function module.TrackEntity(part, distance, color, length)
 						}):Play()
 					end
 				else
-					game["Run Service"]:UnbindFromRenderStep(renderName)
+					RunService:UnbindFromRenderStep(renderName)
 					if module.Enableds[part.Parent.Name] then
 						module.EntityDisable(part.Parent.Name)
-						game.Debris:AddItem(entityUI,0.4)
+						Debris:AddItem(entityUI,0.4)
 					end
 					return
 				end
 			else
 				if module.Enableds[part.Parent.Name] then
 					module.EntityDisable(part.Parent.Name)
-					game.Debris:AddItem(entityUI,0.4)
+					Debris:AddItem(entityUI,0.4)
 				end
 				return
 			end
 		end)
 end
 local staticInfo = TweenInfo.new(0.1, Enum.EasingStyle.Linear, Enum.EasingDirection.InOut, -1, true, 0)
-module.StaticTween1 = game.TweenService:Create(module.UI.Static.s1, staticInfo, {
+module.StaticTween1 = TweenService:Create(module.UI.Static.s1, staticInfo, {
 	["Position"] = UDim2.new(0.49, 0, 0.5, 0)
 })
-module.StaticTween2 = game.TweenService:Create(module.UI.Static.s2, staticInfo, {
+module.StaticTween2 = TweenService:Create(module.UI.Static.s2, staticInfo, {
 	["Position"] = UDim2.new(0.51, 0, 0.5, 0)
 })
 function module.StaticToggle(bool)
