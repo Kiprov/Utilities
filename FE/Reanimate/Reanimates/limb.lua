2ed84ff3c07546d7f5e2d49dcd60f9dd531e04d9
Kiprov <118040079+Kiprov@users.noreply.github.com>
2025-10-24 15:32:17 +0300

    Update limb.lua

--- FE/Reanimate/Reanimates/limb.lua
+++ FE/Reanimate/Reanimates/limb.lua
@@ -8,6 +8,7 @@ end
 --// Below are the settings
 -- SETTINGS --
 local settings = _G
+local fpdh = math.abs(workspace.FallenPartsDestroyHeight + 5)
 
 settings["Use default animations"] = true
 settings["Fake character transparency level"] = 1
@@ -16,14 +17,14 @@ settings["Fake character should collide"] = true
 settings["Parent real character to fake character"] = true
 settings["Respawn character"] = true
 settings["Instant respawn"] = true
-settings["Hide HumanoidRootPart"] = true
+settings["Hide HumanoidRootPart"] = false
 settings["PermaDeath fake character"] = true
 settings["R15 Reanimate"] = false
 settings["Click Fling"] = false
 settings["Anti-Fling"] = true
-settings["Hide RootPart Distance"] = CFrame.new(255, 255, 0)
+settings["Hide RootPart Distance"] = CFrame.new(fpdh, fpdh, 0)
 
 settings["Names to exclude from transparency"] = {}
 --// Settings end
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/somethingsimade/CurrentAngleV4/refs/heads/main/currentanglev2.5.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/somethingsimade/CurrentAngleV4/refs/heads/main/v4.lua"))()
